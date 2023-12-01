a7d3d2765b253822b0c21d5af0ec715d56198a99
markel <100451267@alumnos.uc3m.es>
2023-12-01 10:47:28 +0100

    proper EDA function, there is an error when trying to do a ANN

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    df['sunrise'] = pd.to_datetime(df['sunrise'])
-    df['sunset'] = pd.to_datetime(df['sunset'])
-    df['moonrise'] = pd.to_datetime(df['moonrise'])
-    df['moonset'] = pd.to_datetime(df['moonset'])
